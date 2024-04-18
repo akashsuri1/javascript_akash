02da153997d1e082f1518e926890be77555898f5
Akash suri <123323118+akashsuri1@users.noreply.github.com>
2024-04-18 19:12:43 +0000

    learned datatypes

---
+++ basics/Datatypes.js
@@ -0,0 +1,32 @@
+"use strict";
+// this is to indicate we are using the newer version of javascript and new standard
+// alert("hello"); cannot use it in nodejs
+console.log(3
++ 3) // improve code readibility
+console.log("akash suri")
+// datatypes avaliable in javascript
+let name="akash suri";
+let age=19;
+let isLogged=true;
+let state=null;
+let value;
+// number has range from 2 to 53
+// string store in double quotes ""
+// boolean return true or false if it is present 
+// null => is an standalone value or an object 
+// undefined is an value used to say that value is not defined 
+// symbol is used for unique values mostly in 
+// primitive datatypes
+// react to make sure that both different component are very different from each other
+// while if server cannot respond to an request it can return null stating no value present
+// while the derived datatype include
+// object => 
+console.log(typeof "hitesh")
+console.log(typeof age)
+// null has datatype of object
+console.log(typeof null)
+console.log(typeof undefined)
+// ecma script is an standard formed for writting javascript made by all browser to ensure that it is consistent throughout the development world
+
+
+
