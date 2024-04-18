ba4f59a2ee48d5e31171a9872f61737a87626922
Akash suri <123323118+akashsuri1@users.noreply.github.com>
2024-04-18 18:03:30 +0000

    learning variable was fun

---
+++ basics/variable_constant.js
@@ -0,0 +1,13 @@
+const accountId=13445
+var accountName="Akash suri"
+let accountEmail="akashsuri1979@gmail.com"
+accountCity="jaipur"
+let accountState;
+// it is advisable to declare let variableas they have block definition and not the global declarition
+// also as javascript is not so strict so we can declare an variable without any keyword but is not recommeded as is it can produce any result
+// also the varaible declard but not intialized have an datavalue of undefined
+console.table([accountId,accountName,accountCity,accountEmail,accountState])
+accountName="Vijay suri"
+accountEmail="vijaysuri1979@gmail.com"
+accountCity="jassur khas"
+console.table([accountId,accountName,accountCity,accountEmail,accountState])
