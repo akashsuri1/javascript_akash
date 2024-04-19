d83791ecc1f6ad435128636ef1435c1c0d03e043
Akash suri <123323118+akashsuri1@users.noreply.github.com>
2024-04-19 11:42:45 +0000

    this is new comprassion .js

---
+++ basics/comprassion.js
@@ -0,0 +1,22 @@
+console.log("2">"1")
+console.log("2" == "2")
+console.log(2>1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2!=1)
+console.log(2<1)
+//  when we convert the datatype the main problem arise them
+// some convert both to number while sum to nan
+console.log(1==true)
+console.log(null<0)
+console.log(null == 0)
+console.log(null>= 0)
+// null converted to NaN by == and to 0 by comprassion operator
+console.log(undefined<0)
+console.log(undefined ==0 )
+console.log(undefined >=0 )
+// undefined is always converted To NAN 
+// strict check also compares wether the datatype is also same
+console.log("2"==2)
+console.log(2==="2")
+// the strict does do the type conversion also check the types also
