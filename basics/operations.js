617fe6d287223fea9152b674b4d6ba6007b8ccdf
Akash suri <123323118+akashsuri1@users.noreply.github.com>
2024-04-19 09:09:33 +0000

    please save this edir

---
+++ basics/operations.js
@@ -0,0 +1,32 @@
+let value=3
+let negvalue=-value
+console.log(negvalue)
+console.log(1+2)
+console.log(2-2)
+console.log(2*3)
+console.log(2/3)
+console.log(2**4)
+console.log(89%3)
+let str1="akash"
+let str2=" suri"
+let str3=str1+str2
+console.log(str3)
+console.log("33"-"22")
+
+console.log(typeof( "1"+"2"))
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(typeof (1+2+"2"))
+console.log(typeof(-true))
+console.log(+"")
+
+console.log(3+4*5%3)
+
+let num1,num2,num3
+num1=num2=num3=20;
+console.table([num1,num2,num3])
+
+let gameCounter=100
+let score= ++gameCounter + gameCounter++;
+console.log(gameCounter,score);
+// here the ans will be 202 , 102
